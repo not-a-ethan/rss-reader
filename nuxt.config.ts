1dc48e982d8389c587d55fdd4c1eeac0919734ca
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-12-29 15:14:10 -0500

    Working on getting auth working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  modules: ['@sidebase/nuxt-auth'],
+  auth: {
+    originEnvKey: "NUXT_AUTH_ORIGIN",
+  },
+  runtimeConfig: {
+    authSecret: process.env.NUXT_AUTH_SECRET,
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret,
+    authOrigin: process.env.NUXT_AUTH_ORIGIN
+  }
 });
