ac27217ea91c35436ad0eea831d22975a7a620bc
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-12-30 20:19:37 -0500

    Basic structure of feed page

---
+++ app/pages/feeds.vue
@@ -0,0 +1,39 @@
+<script>
+
+</script>
+
+<template>
+    <div id="container">
+        <div id="feeds">
+            <div class="feed">
+                <h3>Feed #1</h3>
+            </div>
+
+            <div class="feed">
+                <h3>Feed #2</h3>
+            </div>
+        </div>
+
+        <div id="items">
+            <div class="item">
+                <h3>Some title</h3>
+
+                <p>Incididunt culpa eu dolor anim sit proident Lorem aute quis. Est reprehenderit anim magna deserunt ipsum mollit adipisicing esse enim.</p>
+            </div>
+
+            <div class="item">
+                <h3>Some title</h3>
+
+                <p>Incididunt culpa eu dolor anim sit proident Lorem aute quis. Est reprehenderit anim magna deserunt ipsum mollit adipisicing esse enim.</p>
+            </div>
+        </div>
+
+        <div id="content">
+            <p>article content</p>
+        </div>
+    </div>
+</template>
+
+<style>
+    @import url("~/assets/styles/feeds.css")
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  modules: ['@sidebase/nuxt-auth'],
-  auth: {
-    originEnvKey: "NUXT_AUTH_ORIGIN",
-  }
+  modules: [],
 });
