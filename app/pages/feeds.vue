562e99c40ad4edbcbcb50a0d9801088d6df58bd3
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-12-31 21:40:43 -0500

    Made the data display atleast somewhat nicely

--- app/pages/feeds.vue
+++ app/pages/feeds.vue
@@ -12,38 +12,41 @@ import type { FeedsTable, RssItem } from '~~/utils/types';
                         
         items[feeds[i]["id"]] = data.value.items;
     };
-    
-    console.log(items);
+
+    const allItems: RssItem[] = Object.values(items).flat();
+
+    const selectedFeed = reactive({
+        id: 0
+    });
+    const selectedItem = reactive({
+        id: -1
+    });
 </script>
 
 <template>
     <div id="container">
         <div id="feeds">
-            <div class="feed">
-                <h3>Feed #1</h3>
+            <div class="feed" @click="selectedFeed.id = 0">
+                <h3>All feeds</h3>
             </div>
 
-            <div class="feed">
-                <h3>Feed #2</h3>
+            <div v-for="(feed, i) in feeds" :key="feed.id ?? i" class="feed" @click="selectedFeed.id = i">
+                <h3>{{ feed["name"] }}</h3>
             </div>
         </div>
 
         <div id="items">
-            <div class="item">
-                <h3>Some title</h3>
+            <div v-for="(item, i) in allItems" :key="i" class="item" @click="selectedItem.id = i">
+                <h3>{{ item["title"] }}</h3>
 
-                <p>Incididunt culpa eu dolor anim sit proident Lorem aute quis. Est reprehenderit anim magna deserunt ipsum mollit adipisicing esse enim.</p>
-            </div>
-
-            <div class="item">
-                <h3>Some title</h3>
-
-                <p>Incididunt culpa eu dolor anim sit proident Lorem aute quis. Est reprehenderit anim magna deserunt ipsum mollit adipisicing esse enim.</p>
+                <p>{{ item["contentSnippet"] }}</p>
             </div>
         </div>
 
         <div id="content">
-            <p>article content</p>
+            <h1><a target="_blank" :href=allItems[selectedItem.id]?.link>{{ allItems[selectedItem.id]?.title }}</a></h1>
+            
+            <div v-html="allItems[selectedItem.id]?.content"></div>
         </div>
     </div>
 </template>
